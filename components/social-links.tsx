336f32333b661eafa00b22691e5d235f059c5103
Harsh Sharma <sharmahs017@gmail.com>
2025-10-13 17:37:19 +0530

    add x social link on header

--- components/social-links.tsx
+++ components/social-links.tsx
@@ -3,6 +3,7 @@
 import { CiLinkedin } from "react-icons/ci"
 import { FaGithub } from "react-icons/fa";
 import { MdOutlineEmail } from "react-icons/md";
+import { FaSquareXTwitter } from "react-icons/fa6";
 
 type SocialLinksProps = {
   size?: number
@@ -10,6 +11,7 @@ type SocialLinksProps = {
   email?: string
   github?: string
   linkedin?: string
+  xaccount?: string
 }
 
 export default function SocialLinks({
@@ -18,6 +20,7 @@ export default function SocialLinks({
   email = "harshsharma17122005@gmail.com",
   github = "https://github.com/HarshSharma1712",
   linkedin = "https://www.linkedin.com/in/harshsharma0017/",
+  xaccount = "https://x.com/teche_harsh"
 }: SocialLinksProps) {
   const linkClasses =
     "inline-flex items-center justify-center rounded-md border border-border/60 bg-card/50 hover:bg-card transition-colors h-8 w-8 text-muted-foreground hover:text-foreground"
@@ -34,6 +37,11 @@ export default function SocialLinks({
       </a>
       <a href={linkedin} target="_blank" rel="noopener noreferrer" aria-label="LinkedIn" className={linkClasses}>
         <CiLinkedin size={size} aria-hidden="true" />
+        <span className="sr-only">LinkedIn</span>
+      </a>
+
+      <a href={xaccount} target="_blank" rel="noopener noreferrer" aria-label="xaccount" className={linkClasses}>
+        <FaSquareXTwitter size={size} aria-hidden="true" />
         <span className="sr-only">LinkedIn</span>
       </a>
     </div>
